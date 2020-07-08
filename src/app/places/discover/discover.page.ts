2ee6953809d8b6903ee66b8c64331275c7785071
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-08 23:34:33 +0530

    Updates

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,18 @@
+import { place } from './../service/placesinterface';
+import { PlacesService } from './../service/places.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  selector: "app-discover",
+  templateUrl: "./discover.page.html",
+  styleUrls: ["./discover.page.scss"],
 })
 export class DiscoverPage implements OnInit {
+  constructor(private places: PlacesService) {}
 
-  constructor() { }
+  place: place[]=[];
 
   ngOnInit() {
+   this.place= this.places.getplaces();
   }
-
 }
