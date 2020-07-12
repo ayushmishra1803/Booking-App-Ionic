58c5ddbb709934d79751e2b390962260b11ff370
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-13 01:37:10 +0530

    Updates

--- src/app/places/discover/place-details/place-details.page.ts
+++ src/app/places/discover/place-details/place-details.page.ts
@@ -2,7 +2,11 @@ import { PlacesService } from "./../../service/places.service";
 import { place } from "./../../service/placesinterface";
 import { BookComponent } from "./../../book/book.component";
 import { Component, OnInit } from "@angular/core";
-import { ModalController, NavController } from "@ionic/angular";
+import {
+  ModalController,
+  NavController,
+  ActionSheetController,
+} from "@ionic/angular";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
@@ -11,38 +15,55 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["./place-details.page.scss"],
 })
 export class PlaceDetailsPage implements OnInit {
-  places: place
+  places: place;
   constructor(
     private model: ModalController,
     private service: PlacesService,
     private router: ActivatedRoute,
-    private nav:NavController
+    private nav: NavController,
+    private actionsheet: ActionSheetController
   ) {}
 
   ngOnInit() {
-    this.router.paramMap.subscribe(param=>{
+    this.router.paramMap.subscribe((param) => {
       if (!param.has("places")) {
         this.nav.navigateBack(["/places"]);
         return;
       }
       this.places = this.service.getsingleplace(param.get("places"));
-    })
+    });
   }
   book() {
-    this.model
-      .create({
-        component: BookComponent,componentProps:{selectedplaces:this.places}
+    this.actionsheet
+      .create({header:"Choose the Action",
+        buttons: [
+          { text: "selected", handler: () => {this.onbook("select")} },
+          { text: "ramdom", handler: () => {this.onbook("ramdom")} },
+          { text: "cancel", role: "cancel" },
+        ],
       })
       .then((re) => {
         re.present();
-       return re.onDidDismiss()
-      }).then((re)=>{
-        console.log(re.data,re.role);
-        if(re.role==="success"){
+      });
+    
+  }
+  onbook(model:"select" |"ramdom"){
+    console.log(model);
+    
+    this.model
+      .create({
+        component: BookComponent,
+        componentProps: { selectedplaces: this.places },
+      })
+      .then((re) => {
+        re.present();
+        return re.onDidDismiss();
+      })
+      .then((re) => {
+        console.log(re.data, re.role);
+        if (re.role === "success") {
           console.log("Booked");
-          
         }
-        
       });
   }
 }
