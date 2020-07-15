8e6b2b26b21901269c568e4e3915064b8a6c72a5
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-16 01:48:18 +0530

    Updates

--- src/app/places/discover/discover-routing.module.ts
+++ src/app/places/discover/discover-routing.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { DiscoverPage } from './discover.page';
+import { DiscoverPage } from "./discover.page";
 
 const routes: Routes = [
   {
-    path: '',
-    component: DiscoverPage
+    path: "",
+    component: DiscoverPage,
   },
   {
-    path: ':places',
-    loadChildren: () => import('./place-details/place-details.module').then( m => m.PlaceDetailsPageModule)
-  }
+    path: ":places",
+    loadChildren: () =>
+      import("./place-details/place-details.module").then(
+        (m) => m.PlaceDetailsPageModule
+      ),
+  },
 ];
 
 @NgModule({
