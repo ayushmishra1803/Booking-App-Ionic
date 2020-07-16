b419b958e9f683ddf3a33af6c054f9b47bc3d1e1
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-16 22:50:15 +0530

    Updates

--- src/app/places/offer/new-offer/new-offer.page.ts
+++ src/app/places/offer/new-offer/new-offer.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-offer',
-  templateUrl: './new-offer.page.html',
-  styleUrls: ['./new-offer.page.scss'],
+  selector: "app-new-offer",
+  templateUrl: "./new-offer.page.html",
+  styleUrls: ["./new-offer.page.scss"],
 })
 export class NewOfferPage implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  form:FormGroup;
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+      description: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required, Validators.maxLength(180)],
+      }),
+      price: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+    });
   }
-
+  offer() {
+    console.log(this.form);
+    
+  }
 }
