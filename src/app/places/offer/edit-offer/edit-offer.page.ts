01b4f4289459bcfd62e91bdf3de5a8fbb9eef7aa
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-22 18:08:15 +0530

    Updates

--- src/app/places/offer/edit-offer/edit-offer.page.ts
+++ src/app/places/offer/edit-offer/edit-offer.page.ts
@@ -1,25 +1,37 @@
+import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, FormGroupName, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-edit-offer',
-  templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  selector: "app-edit-offer",
+  templateUrl: "./edit-offer.page.html",
+  styleUrls: ["./edit-offer.page.scss"],
 })
 export class EditOfferPage implements OnInit {
-
-  constructor() { }
- form:FormGroup
+  constructor(private Active: ActivatedRoute, private navCtrl: NavController) {}
+  form: FormGroup;
+  
+ 
 
   ngOnInit() {
-    this.form=new FormGroup({
-      title:new FormControl({
-        Validators:[Validators.required]
+    this.Active.paramMap.subscribe((re) => {
+      if (!re.has("placeId")) {
+        this.navCtrl.navigateBack(["places/tabs/offers"]);
+        return;
+      }
+      else{
+        console.log(re);
+        
+      }
+    });
+    this.form = new FormGroup({
+      title: new FormControl({
+        Validators: [Validators.required],
       }),
-      discrition:new FormControl({
-        Validators:[Validators.required]
-      })
-    })
+      discrition: new FormControl({
+        Validators: [Validators.required],
+      }),
+    });
   }
-
 }
